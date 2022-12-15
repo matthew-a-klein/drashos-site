cf7e6b63135cfddf5e1b8742e25331e154998d97
Matthew Klein <matthew-a-klein@github.com>
2022-12-15 12:26:14 +0000

    Create drashah model

--- backend/drashos/admin.py
+++ backend/drashos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Drashah
 # Register your models here.
+
+class DrashahAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+
+admin.site.register(Drashah, DrashahAdmin)
--- backend/drashos/models.py
+++ backend/drashos/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Drashah(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
