0ed33f32eb806eb07f3ae045fc77465b1b3e47de
Matthew Klein <matthew-a-klein@github.com>
2022-12-15 13:49:46 +0000

    Add Drashahview

--- backend/drashos/views.py
+++ backend/drashos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import DrashahSerializer
+from .models import Drashah
+
 # Create your views here.
+
+class DrashahView(viewsets.ModelViewSet):
+    serializer_class = DrashahSerializer
+    queryset = Drashah.objects.all()
