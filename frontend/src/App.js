43bbf1cab7b4d443adf9b6bcf572aae3fa1bd236
Matthew Klein <matthew-a-klein@github.com>
2022-12-15 14:43:20 +0000

    display data to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import React from 'react'
+
+import axios from 'axios';
+import Drashah from './Drashah';
+
+
 export default function App() {
+
+  const [drashosList, setDrashosList] = React.useState([])
+
+  React.useEffect(() => {
+    axios.get('api/drashos').then(function (response) {
+      setDrashosList(response.data)
+    })
+  }, [])
+
+  const drashahItems = drashosList.map(function (drashah) {
+    return (
+      <Drashah title={drashah.title} description={drashah.description} />)
+  })
+
   return (
     <div>
       <h1 className="text-xl text-red-600">
         Hello world!
+        {drashahItems}
       </h1>
     </div>
-
   )
 }
---
+++ frontend/src/Drashah.js
@@ -0,0 +1,11 @@
+export default function Drashah(props) {
+    return (<div>
+        <h1>{props.title}</h1>
+        <br/>
+        <p>{props.description}</p>
+        <hr></hr>
+    </div>
+
+
+    )
+}
