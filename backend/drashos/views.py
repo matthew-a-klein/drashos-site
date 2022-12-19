1388ed64c6494166e55810165707b19052896bff
Matthew Klein <matthew-a-klein@github.com>
2022-12-19 13:26:06 +0000

    Create Api for post only

--- backend/drashos/views.py
+++ backend/drashos/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
 
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .serializers import DrashahSerializer
 from .models import Drashah
 
 # Create your views here.
 
-class DrashahView(viewsets.ModelViewSet):
-    serializer_class = DrashahSerializer
-    queryset = Drashah.objects.all()
+@api_view(['POST'])
+def drashos_api(request, *args, **kwargs):
+    """
+    DRF API View
+    """
+    serializer = DrashahSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
